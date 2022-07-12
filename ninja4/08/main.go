f6c95701023cf45a402d641ae379bab949596ffe
Danpom-desktop <dpom90@gmail.com>
2022-07-12 17:04:08 +0100

    ninja4 ex 8 - removed redundent string[]

--- ninja4/08/main.go
+++ ninja4/08/main.go
@@ -14,9 +14,9 @@ import "fmt"
 
 func main() {
 	m := map[string][]string{
-		`bond_james`:      []string{`Shaken, not stirred`, `Martinis`, `Women`},
-		`moneypenny_miss`: []string{`James Bond`, `Literature`, `Computer Science`},
-		`no_dr`:           []string{`Being evil`, `Ice cream`, `Sunsets`},
+		`bond_james`:      {`Shaken, not stirred`, `Martinis`, `Women`},
+		`moneypenny_miss`: {`James Bond`, `Literature`, `Computer Science`},
+		`no_dr`:           {`Being evil`, `Ice cream`, `Sunsets`},
 	}
 	fmt.Println(m)
 
