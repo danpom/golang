ebde1236ac3d2b039a85e7dfc07d96588c9eea84
Danpom-desktop <dpom90@gmail.com>
2022-07-12 15:09:30 +0100

    adding ex 1 from ninja 4

---
+++ ninja4/01/main.go
@@ -0,0 +1,28 @@
+package main
+
+// Hands-on exercise #1
+// ● Using a COMPOSITE LITERAL:
+// ● create an ARRAY which holds 5 VALUES of TYPE int
+// ● assign VALUES to each index position.
+// ● Range over the array and print the values out.
+// ● Using format printing
+// ○ print out the TYPE of the array
+import "fmt"
+
+func main() {
+
+	intArr := [5]int{1, 2, 3, 4, 5}
+
+	for _, number := range intArr {
+		fmt.Printf("%d\n", number)
+	}
+
+	fmt.Printf("%T", intArr)
+
+	// intArr[0] = 1
+	// intArr[1] = 2
+	// intArr[2] = 3
+	// intArr[3] = 4
+	// intArr[4] = 5
+
+}
