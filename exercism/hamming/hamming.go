c24f686faad7d74d0fc6885fef0818896622ef81
dan <dpom90@gmail.com>
2022-10-14 11:36:22 -0700

    my hamming solution - first iteration

--- exercism/hamming/hamming.go
+++ exercism/hamming/hamming.go
@@ -1,5 +1,29 @@
 package hamming
 
+import "errors"
+
+//calculates the hamming difference between string a and b
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	alen := len(a)
+	blen := len(b)
+
+	// switch {
+	// case a == b && b == "":
+	// 	return 0, errors.New("empty strands")
+	// case :
+
+	// }
+
+	if alen == blen {
+		sum := 0
+		for i := 0; i < alen; i++ {
+			if a[i] != b[i] {
+				sum++
+			}
+		}
+
+		return sum, nil
+	} else {
+		return 0, errors.New("input strings must have the same number characters")
+	}
 }
