863881eafa836e80f3c7746bae40e4be0f1540f1
dan <dpom90@gmail.com>
2022-10-14 13:50:43 -0700

    my raindrops solution - first iteration

--- exercism/raindrops/raindrops.go
+++ exercism/raindrops/raindrops.go
@@ -1,5 +1,25 @@
 package raindrops
 
+import "strconv"
+
+//converts a number in to a string of raindrop sounds depending on whether they are a factoer of 3,5, and/or 7
 func Convert(number int) string {
-	panic("Please implement the Convert function")
+	result := ""
+
+	if number%3 == 0 {
+		result += "Pling"
+	}
+	if number%5 == 0 {
+		result += "Plang"
+	}
+	if number%7 == 0 {
+		result += "Plong"
+	}
+
+	if result == "" {
+		return strconv.Itoa(number)
+	} else {
+		return result
+	}
+
 }
