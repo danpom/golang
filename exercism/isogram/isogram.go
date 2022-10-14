69811b66d29ab63d08e052fd64a55d9eb79dc7eb
dan <dpom90@gmail.com>
2022-10-14 15:27:58 -0700

    my isogram solution - first iteration

--- exercism/isogram/isogram.go
+++ exercism/isogram/isogram.go
@@ -1,5 +1,24 @@
 package isogram
 
+import "strings"
+
+//IsIsogram determines if a word or a phrase is an isogram
 func IsIsogram(word string) bool {
-	panic("Please implement the IsIsogram function")
+	//remove all occurances of whitespace and hyphens
+	word = strings.Replace(word, " ", "", -1)
+	word = strings.Replace(word, "-", "", -1)
+	word = strings.ToLower(word)
+	// initialise a slice of type string to keep track of the letters we come across
+	letters := []rune{}
+
+	// iterate through input word and check if letter exists in our letters slice
+	for _, letter := range word {
+		for i := 0; i < len(letters); i++ {
+			if letters[i] == letter {
+				return false //second occurance of the same letter
+			}
+		}
+		letters = append(letters, letter)
+	}
+	return true // no letters repeated in input phrase
 }
