83633cd71891480d109a244cd85f1e1b857da1f7
dan <dpom90@gmail.com>
2022-10-14 17:02:27 -0700

    my difference_of_squares solution - first iteration

--- exercism/difference-of-squares/difference_of_squares.go
+++ exercism/difference-of-squares/difference_of_squares.go
@@ -1,13 +1,41 @@
 package diffsquares
 
+//SquareOfSum  returns the square of the sum of n numbers from 1 to n
 func SquareOfSum(n int) int {
-	panic("Please implement the SquareOfSum function")
+	result := 0
+	result = SquareOfSumRec((n), result)
+	result = result * result
+
+	return result
 }
 
+//SquareOfSumRec is a helper function which uses recursion to calculate the square of a sum of numbers from 1 to n
+func SquareOfSumRec(n, sum int) int {
+	if n > 0 {
+		sum += n
+		return SquareOfSumRec((n - 1), sum)
+	}
+	return sum
+}
+
+//SumOfSquares returns the sum of the square of all numbers from 1 to n
 func SumOfSquares(n int) int {
-	panic("Please implement the SumOfSquares function")
+	result := 0
+	result = SumOfSquaresRec(n, result)
+
+	return result
 }
 
+//SumOfSquaresRec is a helper function which uses recursion to calculate the sum of the square of each number from 1 to n
+func SumOfSquaresRec(n, sum int) int {
+	if n > 0 {
+		sum += n * n
+		return SumOfSquaresRec((n - 1), sum)
+	}
+	return sum
+}
+
+//Difference returns the difference between the SquareOfSum and the SumOfSquares of a number n
 func Difference(n int) int {
-	panic("Please implement the Difference function")
+	return SquareOfSum(n) - SumOfSquares(n)
 }
