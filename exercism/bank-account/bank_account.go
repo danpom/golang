c9b21ea1a04dd01d82d953c71d136e5929cfa41c
dan <dpom90@gmail.com>
2022-11-14 17:19:10 -0800

    my bank_account - first iteration

--- exercism/bank-account/bank_account.go
+++ exercism/bank-account/bank_account.go
@@ -1,19 +1,57 @@
 package account
 
+import (
+	"math"
+	"sync"
+)
+
 // Define the Account type here.
+type Account struct {
+	balance int64
+	isOpen  bool
+	mu      sync.Mutex
+}
 
 func Open(amount int64) *Account {
-	panic("Please implement the Open function")
+	if amount < 0 {
+		return nil
+	} else {
+		return &Account{balance: amount, isOpen: true}
+	}
 }
 
 func (a *Account) Balance() (int64, bool) {
-	panic("Please implement the Balance function")
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	if !a.isOpen {
+		//return a.balance, false
+		return 0, false
+	}
+	return a.balance, true
 }
 
 func (a *Account) Deposit(amount int64) (int64, bool) {
-	panic("Please implement the Deposit function")
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	switch {
+	case !a.isOpen:
+		return a.balance, false
+	case amount < 0 && float64(a.balance) < math.Abs(float64(amount)):
+		return a.balance, false
+	default:
+		a.balance += amount
+		return a.balance, true
+	}
 }
 
 func (a *Account) Close() (int64, bool) {
-	panic("Please implement the Close function")
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	if !a.isOpen {
+		return a.balance, false
+	}
+	closingBalance := a.balance
+	a.balance = 0
+	a.isOpen = false
+	return closingBalance, true
 }
