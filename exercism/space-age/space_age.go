9edfdde2a5c6f18bc12cca533f225486730ae5f9
dan <dpom90@gmail.com>
2022-11-16 12:53:13 -0800

    my space age - first iteration

--- exercism/space-age/space_age.go
+++ exercism/space-age/space_age.go
@@ -3,5 +3,27 @@ package space
 type Planet string
 
 func Age(seconds float64, planet Planet) float64 {
-	panic("Please implement the Age function")
+	//calculate Earth years
+	ey := seconds / 31557600
+	//return planet years based on input
+	switch planet {
+	case "Mercury":
+		return ey / 0.2408467
+	case "Venus":
+		return ey / 0.61519726
+	case "Earth":
+		return ey
+	case "Mars":
+		return ey / 1.8808158
+	case "Jupiter":
+		return ey / 11.862615
+	case "Saturn":
+		return ey / 29.447498
+	case "Uranus":
+		return ey / 84.016846
+	case "Neptune":
+		return ey / 164.79132
+	default:
+		return -1
+	}
 }
