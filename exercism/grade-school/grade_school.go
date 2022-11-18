834abbcbc44375eaea8e804683530ef2426de37c
dan <dpom90@gmail.com>
2022-11-18 12:29:58 -0800

    my grade-school - first iteration

--- exercism/grade-school/grade_school.go
+++ exercism/grade-school/grade_school.go
@@ -1,26 +1,61 @@
 package school
 
+import "sort"
+
 // Define the Grade and School types here.
 
 type Grade struct {
-	student []string
-	level   int
+	level    int
+	students []string
 }
 
-type School map[int]Grade
+//type School map[int]*Grade
+type School map[int]*Grade
 
 func New() *School {
 	return &School{}
 }
 
 func (s *School) Add(student string, grade int) {
-	(*s)[grade] = append((*s)[grade], student)
+	if _, ok := (*s)[grade]; !ok {
+		(*s)[grade] = &Grade{
+			level:    grade,
+			students: []string{student},
+		}
+	} else {
+		(*s)[grade].students = append((*s)[grade].students, student)
+	}
 }
 
 func (s *School) Grade(level int) []string {
-	return (*s)[level]
+	for k := range *s {
+		if k == level {
+			return (*s)[level].students
+		}
+	}
+	return []string{}
+
 }
 
 func (s *School) Enrollment() []Grade {
-	return
+	var res []Grade
+	for _, v := range *s {
+		//sort students alphabetically
+		sortStudents := v.students
+		sort.Slice(sortStudents, func(i, j int) bool {
+			return sortStudents[i] < sortStudents[j]
+		})
+		g := Grade{
+			students: sortStudents,
+			level:    v.level,
+		}
+		res = append(res, g)
+	}
+	//sort grades ascending
+	sort.Slice(res, func(i, j int) bool {
+		return res[i].level < res[j].level
+	})
+
+	return res
+
 }
