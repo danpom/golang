dee75d07605068ea1af5957d2d396695874ffbf7
dan <dpom90@gmail.com>
2022-12-05 12:51:11 -0800

    my pangram.go solution - first iteration

--- exercism/pangram/pangram.go
+++ exercism/pangram/pangram.go
@@ -1,5 +1,29 @@
 package pangram
 
+import "unicode"
+
+type FreqMap map[rune]int
+
 func IsPangram(input string) bool {
-	panic("Please implement the IsPangram function")
+	m := FreqMap{}
+
+	for _, v := range input {
+		v = unicode.ToLower(v)
+		if IsLetter(v) {
+			m[v]++
+		}
+		if len(m) == 26 {
+			return true
+		}
+
+	}
+	return false
+}
+
+func IsLetter(r rune) bool {
+	if r < 'a' || r > 'z' {
+		return false
+	}
+
+	return true
 }
