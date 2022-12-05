5fdd0cc53b43a95d587fa83b52bf972b4cfffc6b
dan <dpom90@gmail.com>
2022-12-05 14:48:24 -0800

    my sum-of-multiples solution - first iteration

--- exercism/sum-of-multiples/sum_of_multiples.go
+++ exercism/sum-of-multiples/sum_of_multiples.go
@@ -1,5 +1,45 @@
 package summultiples
 
+import "sort"
+
 func SumMultiples(limit int, divisors ...int) int {
-	panic("Please implement the SumMultiples function")
+	//var multiples []int
+	sum := 0
+	if len(divisors) < 1 {
+		return 0
+	}
+	if len(divisors) == 1 && divisors[0] == 0 {
+		return 0
+	}
+
+	sort.Slice(divisors, func(i, j int) bool {
+		return divisors[i] < divisors[j]
+	})
+
+	//index of first non zero divisor
+	ind := 0
+	for i, v := range divisors {
+		if v > 0 {
+			ind = i
+			break
+		}
+	}
+
+	divisors = divisors[ind:]
+
+	for i := divisors[0]; i < limit; i++ {
+		isMultiple := false
+		for _, v := range divisors {
+			if i%v == 0 {
+				isMultiple = true
+				break
+			}
+		}
+		if isMultiple {
+			//multiples = append(multiples, i)
+			sum += i
+		}
+	}
+
+	return sum
 }
