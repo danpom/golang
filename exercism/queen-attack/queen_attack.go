94886cdbc947bde2fddd7100529581c7512c96e7
dan <dpom90@gmail.com>
2022-12-05 13:47:04 -0800

    my queen-attack solution - first iteration

--- exercism/queen-attack/queen_attack.go
+++ exercism/queen-attack/queen_attack.go
@@ -1,5 +1,38 @@
 package queenattack
 
+import "errors"
+
 func CanQueenAttack(whitePosition, blackPosition string) (bool, error) {
-	panic("Please implement the CanQueenAttack function")
+
+	if whitePosition == "" || blackPosition == "" {
+		return false, errors.New("empty position")
+	}
+
+	m := map[string]int{"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8}
+
+	lw := string(whitePosition[0])
+	lwi := m[lw]
+	nw := int(whitePosition[1] - '0')
+
+	lb := string(blackPosition[0])
+	lbi := m[lb]
+	nb := int(blackPosition[1] - '0')
+
+	switch {
+	case len(whitePosition) != 2 || len(blackPosition) != 2:
+		return false, errors.New("invalid position")
+	case nw < 1 || nw > 8 || nb < 1 || nb > 8 || lw < "a" || lw > "h" || lb < "a" || lb > "h":
+		return false, errors.New("position off board")
+	case whitePosition == blackPosition:
+		return false, errors.New("same square")
+	case lw == lb:
+		return true, nil
+	case nw == nb:
+		return true, nil
+	case (lwi-nw == lbi-nb) || (lwi+nw == lbi+nb):
+		return true, nil
+	default:
+		return false, nil
+	}
+
 }
