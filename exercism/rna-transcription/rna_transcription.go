e44189e7de8f27db2aa046a5b593618b36c5687a
dan <dpom90@gmail.com>
2023-01-11 15:09:35 -0800

    rna-transcription -solution - first iteration

--- exercism/rna-transcription/rna_transcription.go
+++ exercism/rna-transcription/rna_transcription.go
@@ -1,5 +1,16 @@
 package strand
 
+var dtr = map[rune]rune{'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
+
+// G -> C
+// C -> G
+// T -> A
+// A -> U
+
 func ToRNA(dna string) string {
-	panic("Please implement the ToRNA function")
+	var rna string
+	for _, v := range dna {
+		rna += string(dtr[v])
+	}
+	return rna
 }
