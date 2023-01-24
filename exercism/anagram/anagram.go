3f2e2d7ef26d809a9b59a9c52c7d0e037863af6c
dan <dpom90@gmail.com>
2023-01-23 18:32:50 -0800

    anagram - solution - first iteration

--- exercism/anagram/anagram.go
+++ exercism/anagram/anagram.go
@@ -1,5 +1,35 @@
 package anagram
 
+import (
+	"sort"
+	"strings"
+)
+
 func Detect(subject string, candidates []string) []string {
-	panic("Please implement the Detect function")
+	//slice of string to store anagrams of string selected from candidates
+	var output []string
+
+	//sort subject alphabetically
+	ss := SortString(subject)
+
+	//sort each candidate string and compare with the sorted input subject string
+	for _, v := range candidates {
+		//words are not anagrams of themselves && sorted strings are the same
+		if !strings.EqualFold(subject, v) && SortString(v) == ss {
+			output = append(output, v)
+		}
+	}
+	return output
+
+}
+
+// SortString takes a string and returns the same string sorted alphabetically
+func SortString(s string) string {
+	r := []rune(strings.ToLower(s))
+
+	sort.Slice(r, func(i, j int) bool {
+		return r[i] < r[j]
+	})
+
+	return string(r)
 }
