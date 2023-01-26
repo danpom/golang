d450b26dd83fa956801c93fe2a5b97625918dee3
dan <dpom90@gmail.com>
2023-01-26 12:56:42 -0800

    reverse-string - solution - first iteration

--- exercism/reverse-string/reverse_string.go
+++ exercism/reverse-string/reverse_string.go
@@ -1,5 +1,10 @@
 package reverse
 
 func Reverse(input string) string {
-	panic("Please implement the Reverse function")
+	var output string
+
+	for _, v := range input {
+		output = string(v) + output
+	}
+	return output
 }
