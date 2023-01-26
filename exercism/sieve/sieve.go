90defbdd0368159935b9e6e277e381e1034747c0
dan <dpom90@gmail.com>
2023-01-26 14:54:44 -0800

    sieve - solution - first iteration

--- exercism/sieve/sieve.go
+++ exercism/sieve/sieve.go
@@ -1,5 +1,30 @@
 package sieve
 
-func Sieve(limit int) []int {
-	panic("Please implement the Sieve function")
+func Sieve(n int) []int {
+	var output []int
+	//create a boolean array
+	prime := make([]bool, n+1)
+
+	//initalise all values as true
+	for i := 0; i <= n; i++ {
+		prime[i] = true
+	}
+
+	for p := 2; p*p <= n; p++ {
+		// If prime[p] unchanged then it is a prime
+		if prime[p] {
+			//Update all multiples of p
+			for i := p * p; i <= n; i += p {
+				prime[i] = false
+			}
+		}
+	}
+
+	//return all prime numbers up to n
+	for i := 2; i <= n; i++ {
+		if prime[i] {
+			output = append(output, i)
+		}
+	}
+	return output
 }
