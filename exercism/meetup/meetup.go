9eb0c67809be15649121dc41b9c490c46a5a4fe6
dan <dpom90@gmail.com>
2023-01-27 12:36:22 -0800

    meetup - solution - first iteration

--- exercism/meetup/meetup.go
+++ exercism/meetup/meetup.go
@@ -2,8 +2,50 @@ package meetup
 
 import "time"
 
-// Define the WeekSchedule type here.
+type WeekSchedule int
 
+const (
+	First = WeekSchedule(iota)
+	Second
+	Third
+	Fourth
+	Teenth
+	Last
+)
+
+//"First Monday of January 2022"
+//Day returns the day of the week for a meeting for the  given restrictions
 func Day(wSched WeekSchedule, wDay time.Weekday, month time.Month, year int) int {
-	panic("Please implement the Day function")
+	var options []int
+
+	date := time.Date(year, month, 1, 0, 0, 0, 0, time.FixedZone("UTC", 0))
+
+	for date.Month() == month {
+		if date.Weekday() == wDay {
+			options = append(options, date.Day())
+		}
+		date = date.AddDate(0, 0, 1) // increment 1 day
+	}
+
+	switch wSched {
+	case First:
+		return options[0]
+	case Second:
+		return options[1]
+	case Third:
+		return options[2]
+	case Fourth:
+		return options[3]
+	case Last:
+		return options[len(options)-1]
+	case Teenth:
+		for _, v := range options {
+			if v > 12 {
+				return v
+			}
+		}
+	}
+
+	panic("Invalid wSched")
+
 }
